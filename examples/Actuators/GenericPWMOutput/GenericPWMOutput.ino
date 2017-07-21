1be6ac21344ab6da5b9c8f0f94a67555eb15f037
jburhenn <jburhenn@mydevices.com>
2017-07-21 13:25:07 -0600

    Update analogWrite sketches to use 0 to 255 values the server now sends.

--- examples/Actuators/GenericPWMOutput/GenericPWMOutput.ino
+++ examples/Actuators/GenericPWMOutput/GenericPWMOutput.ino
@@ -8,19 +8,20 @@ The Cayenne Library is required to run this sketch. If you have not already done
 Steps:
 1. In the Cayenne Dashboard add a new Generic PWM Output.
 2. Select a virtual pin number.
-2. Select a slider widget.
-3. Set min value to 0 and max value of 1.
-4. Set PWM_VIRTUAL_PIN to the virtual pin number you selected.
-3. Attach an output device to a digital PWM pin (3, 5, 6, 9, 10, and 11 on most Arduino boards).
-6. Set PWM_DIGITAL_PIN to the digital PWM pin number you selected.
-7. Set the token variable to match the Arduino token from the Dashboard.
-8. Compile and upload this sketch.
-9. Once the Arduino connects to the Dashboard you can use the slider to change PWM values.
+3. Select a slider widget.
+4. Set the slider widget min value to 0 and max value of 255.
+5. Set PWM_VIRTUAL_PIN to the virtual pin number you selected.
+6. Attach an output device to a digital PWM pin (3, 5, 6, 9, 10, and 11 on most Arduino boards).
+7. Set PWM_DIGITAL_PIN to the digital PWM pin number you selected.
+8. Set the token variable to match the Arduino token from the Dashboard.
+9. Compile and upload this sketch.
+10. Once the Arduino connects to the Dashboard you can use the slider to change PWM values.
 */
 
 
 #define CAYENNE_PRINT Serial  // Comment this out to disable prints and save space
 #include <CayenneEthernet.h>
+
 #define PWM_VIRTUAL_PIN 1
 #define PWM_DIGITAL_PIN 3
 
@@ -36,8 +37,8 @@ void setup()
 CAYENNE_IN(PWM_VIRTUAL_PIN)
 {
 	// get value sent from dashboard
-	int currentValue = getValue.asInt(); // 0 to 1023
-	analogWrite(PWM_DIGITAL_PIN, currentValue / 4); // must be from 0 to 255
+	int currentValue = getValue.asInt(); // 0 to 255
+	analogWrite(PWM_DIGITAL_PIN, currentValue); // analogWrite accepts a value from 0 to 255
 }
 
 void loop()
